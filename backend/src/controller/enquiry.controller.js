a058520853c6a487cd0dfafba908b5cc77ba94be
rahathussain22 <rahathussain2323@gmail.com>
2025-11-24 16:45:02 +0500

    Implemented Role base Access in enquiry controller

--- backend/src/controller/enquiry.controller.js
+++ backend/src/controller/enquiry.controller.js
@@ -7,9 +7,9 @@ import { Staff } from "../model/staff.model.js";
 
 
 const addEnquiry = asyncHandler(async (req, res) => {
-    const { name, contact, remark = "", followUp, category, creatorId, currentUser} = req.body;
+    const { name, contact, remark = "", followUp, category, creatorId, currentUser } = req.body;
     console.log(req.body)
-    if (!name || !contact || !followUp || !creatorId || !currentUser ) {
+    if (!name || !contact || !followUp || !creatorId || !currentUser) {
         throw new APIError(400, "Provide all required fields");
     }
 
@@ -33,70 +33,70 @@ const addEnquiry = asyncHandler(async (req, res) => {
             throw new APIError(403, "User is not allowed to add enquiry.")
         }
         const duplicate = await Enquiry.exists({
-        name,
-        contact,
-        followUp,
-        category,
-        status: "open"
-    });
+            name,
+            contact,
+            followUp,
+            category,
+            status: "open"
+        });
 
-    if (duplicate) {
-        throw new APIError(409, "Enquiry already exists");
-    }
+        if (duplicate) {
+            throw new APIError(409, "Enquiry already exists");
+        }
 
-    const newEnquiry = await Enquiry.create({
-        
-        name,
-        contact,
-        remark,
-        followUp,
-        category,
-        status: "open",
-        createdBy: creator.createdBy,
-        staffId: creatorId
-    });
+        const newEnquiry = await Enquiry.create({
 
-    res.status(201).json({
-        message: "Enquiry added successfully",
-        data: newEnquiry
-    });
+            name,
+            contact,
+            remark,
+            followUp,
+            category,
+            status: "open",
+            createdBy: creator.createdBy,
+            staffId: creatorId
+        });
+
+        res.status(201).json({
+            message: "Enquiry added successfully",
+            data: newEnquiry
+        });
 
     }
     else {
         creator = await Admin.findOne({ _id: creatorId })
-   
 
-    if (!creator) {
-        throw new APIError(404, "No Admin Found");
-    }
 
-    const duplicate = await Enquiry.exists({
-        name,
-        contact,
-        followUp,
-        category,
-        status: "open"
-    });
+        if (!creator) {
+            throw new APIError(404, "No Admin Found");
+        }
 
-    if (duplicate) {
-        throw new APIError(409, "Enquiry already exists");
-    }
+        const duplicate = await Enquiry.exists({
+            name,
+            contact,
+            followUp,
+            category,
+            status: "open"
+        });
 
-    const newEnquiry = await Enquiry.create({
-        
-        name,
-        contact,
-        remark,
-        followUp,
-        category,
-        status: "open",
-        createdBy: creatorId
-    });
+        if (duplicate) {
+            throw new APIError(409, "Enquiry already exists");
+        }
 
-    res.status(201).json({
-        message: "Enquiry added successfully",
-        data: newEnquiry
-    });
+        const newEnquiry = await Enquiry.create({
+
+            name,
+            contact,
+            remark,
+            followUp,
+            category,
+            status: "open",
+            createdBy: creatorId
+        });
+
+        res.status(201).json({
+            message: "Enquiry added successfully",
+            data: newEnquiry
+        });
 
     }
 });
@@ -140,25 +140,38 @@ const deleteEnquiry = asyncHandler(async (req, res) => {
     let user;
     if (currentUser == "Staff") {
         user = await Staff.findOne({ _id: id })
+        if (!user) {
+            throw new APIError(400, "Staff not found")
+        }
         if (user?.permission !== "all") {
             throw new APIError(403, "User is not allowed to delete enquiry.")
         }
+        const result = await Enquiry.deleteOne({ _id: enquiryId })
+
+        if (result.deletedCount === 0) {
+            throw new APIError(404, "Enquiry not found");
+        }
+        res.status(200).json({
+            message: "Deleted successfully"
+        });
     }
     else {
         user = await Admin.findOne({ _id: id })
+
+        if (!user) {
+            throw new APIError(404, "User not found")
+        }
+        const result = await Enquiry.deleteOne({ _id: enquiryId })
+
+        if (result.deletedCount === 0) {
+            throw new APIError(404, "Enquiry not found");
+        }
+        res.status(200).json({
+            message: "Deleted successfully"
+        })
     }
 
-    if (!user) {
-        throw new APIError(404, "User not found")
-    }
-    const result = await Enquiry.deleteOne({ _id: enquiryId })
 
-    if (result.deletedCount === 0) {
-        throw new APIError(404, "Enquiry not found");
-    }
-    res.status(200).json({
-        message: "Deleted successfully"
-    })
 })
 
 const updateEnquiry = asyncHandler(async (req, res) => {
@@ -179,28 +192,45 @@ const updateEnquiry = asyncHandler(async (req, res) => {
     let user;
     if (currentUser == "Staff") {
         user = await Staff.findOne({ _id: userId })
-        if (user?.permission !== "all" || user?.permission !== "view+add+update") {
-            throw new APIError(403, "User is not allowed to update enquiry.")
+        console.log("permission: ", user.permission);
+        if (!user) {
+            throw new APIError(400, "Staff not found")
         }
+        if (user.permission !== "all" && user.permission !== "view+add+update") {
+            throw new APIError(403, "User is not allowed to update enquiry.");
+        }
+        const updatedEnquiry = await Enquiry.findById(
+            enquiryId
+        );
+        if (updatedEnquiry.status === "closed") {
+            throw new APIError(402, "Status is closed and cannot be changed")
+        }
+        updatedEnquiry.status = status;
+        updatedEnquiry.updatedBy = userId;
+        await updatedEnquiry.save();
+        res.status(200).json({
+            message: "Updated successfully",
+            enquiry: updatedEnquiry
+        });
     }
     else {
         user = await Admin.findOne({ _id: userId })
+
+        if (!user) {
+            throw new APIError(404, "User not found")
+        }
+
+        const updatedEnquiry = await Enquiry.findByIdAndUpdate(
+            enquiryId,
+            { $set: { status: status } },
+            { new: true, runValidators: true }
+        );
+
+        res.status(200).json({
+            message: "Updated successfully",
+            enquiry: updatedEnquiry
+        });
     }
-    if (!user) {
-        throw new APIError(404, "User not found")
-    }
-
-    const updatedEnquiry = await Enquiry.findByIdAndUpdate(
-        enquiryId,
-        { $set: { status: status } },
-        { new: true, runValidators: true }
-    );
-
-    res.status(200).json({
-        message: "Updated successfully",
-        enquiry: updatedEnquiry
-    });
-
 })
 
 export { addEnquiry, getEnquiries, deleteEnquiry, updateEnquiry }
--- backend/src/route/enquiry.route.js
+++ backend/src/route/enquiry.route.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import { addEnquiry, getEnquiries, deleteEnquiry, updateEnquiry } from "../controller/enquiry.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 const enquiryRouter = Router();
-// enquiryRouter.use(authMiddleware);
+enquiryRouter.use(authMiddleware);
 enquiryRouter.route('/addEnquiry').post(addEnquiry)
 enquiryRouter.route('/getEnquiries/:creatorId').get( getEnquiries)
 enquiryRouter.route("/deleteEnquiry").delete(deleteEnquiry)
-enquiryRouter.route("/update").patch(updateEnquiry)
+enquiryRouter.route("/updateEnquiry").patch(updateEnquiry)
 export {enquiryRouter}
